d329da741bc698ae17290ef50b8232df04abef33
SteephanSteve <40833438+SteephanSteve@users.noreply.github.com>
2018-08-22 19:23:47 +0530

    Update common_string.py

--- common_string.py
+++ common_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-a,b=raw_input().split(' ')
+s,x=raw_input().split(' ')
 c=''
-for i in b:
-    if i in a:
+for i in x:
+    if i in s:
         if i not in c:
             c=c+i
 print c
